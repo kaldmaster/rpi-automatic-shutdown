f64e960019528dcdbd455dd0d7a68b0e61f6f9ab
Martin Kaldma <martin.kaldma@exsitec.se>
2021-06-27 10:57:41 +0200

    Adding alert parameter

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ let DEBOUNCE_TIME_MS = 300000; // Debounce for 300 ms (max)
 const inputPin = new Gpio(GPIO_PIN, {
     mode: Gpio.INPUT,
     pullUpDown: Gpio.PUD_DOWN,
-    edge: Gpio.EITHER_EDGE
+    alert: true
+    // edge: Gpio.EITHER_EDGE
 });
 
 inputPin.glitchFilter(DEBOUNCE_TIME_MS); 
