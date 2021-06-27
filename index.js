d1d8f45406204dfb2bdc2c538f8dfa11e79b5c27
Martin Kaldma <martin.kaldma@exsitec.se>
2021-06-27 11:08:49 +0200

    Adding shutwodn methods.

--- index.js
+++ index.js
@@ -7,13 +7,36 @@ const inputPin = new Gpio(GPIO_PIN, {
     mode: Gpio.INPUT,
     pullUpDown: Gpio.PUD_DOWN,
     alert: true
-    // edge: Gpio.EITHER_EDGE
 });
 
-inputPin.glitchFilter(DEBOUNCE_TIME_MS); 
+// Define child process and execute method
+var exec = require('child_process').exec;
 
+function execute(command, callback) {
+    exec(command, function(error, stdout, stderr){ callback(stdout); });
+}
+
+function shutdown() {
+    console.log('Shutting down...');
+    // execute('shutdown -r now', function(callback){
+    //     console.log(callback);
+    // });
+}
+
+// Check current state of power
+let currentLevel = inputPin.digitalRead();
+console.log(`GPIO ${GPIO_PIN} startup level:  ${currentLevel}`);
+if (!currentLevel) {
+    shutdown();
+}
+
+// Trigger on changes
+inputPin.glitchFilter(DEBOUNCE_TIME_MS); 
 inputPin.on('alert', (level, tick) => {
-    console.log(`GPIO ${GPIO_PIN} interrupted, level now:  ${level}`);
+    console.log(`GPIO ${GPIO_PIN}, level now:  ${level}`);
+    if (!currentLevel) {
+        shutdown();
+    }
 });
 
 console.log(`Listening for changes on GPIO ${GPIO_PIN}...`);
