51ac49c99776654ece71b5aa238e7b3825b682a4
Martin Kaldma <martin.kaldma@exsitec.se>
2021-06-27 10:55:28 +0200

    Changed logging

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const inputPin = new Gpio(GPIO_PIN, {
     edge: Gpio.EITHER_EDGE
 });
 
-//inputPin.glitchFilter(DEBOUNCE_TIME_MS); 
+inputPin.glitchFilter(DEBOUNCE_TIME_MS); 
 
 inputPin.on('alert', (level, tick) => {
-    console.log('GPIO ${GPIO_PIN} interrupted, level now:  ${level}');
+    console.log(`GPIO ${GPIO_PIN} interrupted, level now:  ${level}`);
 });
 
-console.log('Listening for changes on GPIO ${GPIO_PIN}...');
+console.log(`Listening for changes on GPIO ${GPIO_PIN}...`);
