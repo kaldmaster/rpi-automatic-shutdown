56c18299a36bdb7fccbccb41d54eac9dc82ca31a
Martin Kaldma <martin.kaldma@exsitec.se>
2021-06-27 10:52:36 +0200

    Adding debounce for 500 ms as variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Gpio = require('pigpio').Gpio;
 
 let GPIO_PIN = 17;
-let DEBOUNCE_TIME_MS = 500000;
+let DEBOUNCE_TIME_MS = 300000; // Debounce for 300 ms (max)
 
 const inputPin = new Gpio(GPIO_PIN, {
     mode: Gpio.INPUT,
@@ -9,7 +9,7 @@ const inputPin = new Gpio(GPIO_PIN, {
     edge: Gpio.EITHER_EDGE
 });
 
-inputPin.glitchFilter(DEBOUNCE_TIME_MS); // Debounce for 500 ms
+inputPin.glitchFilter(DEBOUNCE_TIME_MS); 
 
 inputPin.on('alert', (level, tick) => {
     console.log('GPIO ${GPIO_PIN} interrupted, level now:  ${level}');
